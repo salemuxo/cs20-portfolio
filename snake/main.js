65e2b7f0585c7d4cab9b0a2da98767227e5359a6
Salem <62039916+sufffix@users.noreply.github.com>
2022-10-20 09:58:44 -0600

    added game over screen, added high score storage, changed direction check

--- snake/main.js
+++ snake/main.js
@@ -7,14 +7,18 @@ cnv.height = 600;
 ctx.font = "40px Helvetica";
 ctx.textAlign = "center";
 const fps = 10;
+let score = 0;
 let scoreOut = document.getElementById("scoreOut");
+let highScore = localStorage.getItem("highscore");
+let highScoreOut = document.getElementById("highScoreOut");
+highScoreOut.innerHTML = highScore;
 
 // OBJECTS
 
 const player = {
   w: 25,
   h: 25,
-  xSpeed: 25,
+  xSpeed: 0,
   ySpeed: 0,
   speed: 25,
   alive: true,
@@ -27,7 +31,7 @@ let nextPrize = {
   y: 0,
 };
 
-let safeBounds = {
+const safeBounds = {
   xmin: 0,
   xmax: cnv.width - player.w,
   ymin: 0,
@@ -39,8 +43,19 @@ ranPrize();
 // animate
 requestAnimationFrame(animate);
 function animate() {
-  moveSeg();
-  if (isSnakeDead()) return;
+  moveSnake();
+
+  // if snake dead -> display end screen, store highscore if higher than old highscore
+  if (isSnakeDead()) {
+    player.alive = false;
+    endScreen();
+    if (isHighScore()) {
+      highScore = score;
+      localStorage.setItem("highscore", highScore);
+      highScoreOut.innerHTML = highScore;
+    }
+    return;
+  }
 
   snake.forEach(checkPrize);
 
@@ -50,7 +65,7 @@ function animate() {
 
   // set framerate to fps
   setTimeout(() => {
-    if (player.alive) requestAnimationFrame(animate);
+    requestAnimationFrame(animate);
   }, 1000 / fps);
 }
 
@@ -59,14 +74,24 @@ function animate() {
 // key listener
 document.addEventListener("keydown", keyPressHandler);
 function keyPressHandler(event) {
+  // if player is alive, within safe bounds and not going opposite / same direction already, change xs/ys
   if (player.alive && !event.repeat) {
-    if (event.key == "w" || event.key == "ArrowUp")
+    if ((event.key == "w" || event.key == "ArrowUp") && player.ySpeed === 0)
       changeSpeed(0, -player.speed);
-    else if (event.key == "a" || event.key == "ArrowLeft")
+    else if (
+      (event.key == "a" || event.key == "ArrowLeft") &&
+      player.xSpeed === 0
+    )
       changeSpeed(-player.speed, 0);
-    else if (event.key == "s" || event.key == "ArrowDown")
+    else if (
+      (event.key == "s" || event.key == "ArrowDown") &&
+      player.ySpeed === 0
+    )
       changeSpeed(0, player.speed);
-    else if (event.key == "d" || event.key == "ArrowRight")
+    else if (
+      (event.key == "d" || event.key == "ArrowRight") &&
+      player.xSpeed === 0
+    )
       changeSpeed(player.speed, 0);
   }
 }
@@ -112,15 +137,17 @@ function clearCnv(clr) {
   ctx.fillRect(0, 0, cnv.width, cnv.height);
 }
 
-// draw player at player location
+// draw each segment at location in array
 function drawPlr() {
   snake.forEach(function (segment) {
     ctx.fillStyle = "black";
+    ctx.strokeStyle = "white";
+    ctx.strokeRect(segment.x, segment.y, player.w, player.h);
     ctx.fillRect(segment.x, segment.y, player.w, player.h);
   });
 }
 
-// check if snake has collided with itself or wall -> return false
+// check if snake has collided with itself or wall -> return true
 function isSnakeDead() {
   for (let i = 4; i < snake.length; i++) {
     const collided = snake[i].x === snake[0].x && snake[i].y === snake[0].y;
@@ -134,16 +161,34 @@ function isSnakeDead() {
   return hitLeft || hitRight || hitTop || hitBottom;
 }
 
-function moveSeg() {
+// move head to next tile, if got prize dont delete last segment
+function moveSnake() {
   const head = newSegment(
     snake[0].x + player.xSpeed,
     snake[0].y + player.ySpeed
   );
   snake.unshift(head);
-  if (checkPrize(head)) ranPrize();
-  else snake.pop();
+  if (checkPrize(head)) {
+    ranPrize();
+    score++;
+    scoreOut.innerHTML = score;
+  } else snake.pop();
 }
 
-function addSeg() {
-  snake.push(newSegment(snake[snake.length - 1].x));
+// check if score > highScore -> return true
+function isHighScore() {
+  if (score > +highScore || isNaN(+highScore)) return true;
+  else return false;
+}
+
+// create end screen
+function endScreen() {
+  clearCnv("white");
+
+  clearCnv("#9c0a00");
+  ctx.fillStyle = "white";
+  ctx.fillText("Game Over!", cnv.width / 2, 200);
+  if (isHighScore()) {
+    ctx.fillText(`New High Score: ${score}`, cnv.width / 2, 400);
+  }
 }
