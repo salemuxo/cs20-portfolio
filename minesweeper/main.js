088bcc07ab78705bf07050d8b10d1ad067244ee8
Salem <62039916+sufffix@users.noreply.github.com>
2023-01-03 16:24:27 -0700

    edited home page, added mine generation

--- minesweeper/main.js
+++ minesweeper/main.js
@@ -1,19 +1,26 @@
+// global variables
 let tilesX = 9;
 let tilesY = 9;
 let tilesTotal = tilesX * tilesY;
+let tileSize = 50;
+let mineNum = 10;
+let isGameStarted = false;
 
-let cnv = document.getElementById("gameCnv");
-let ctx = cnv.getContext("2d");
-cnv.width = 450;
-cnv.height = 450;
-
+// create array with tiles
 const tiles = [];
 for (let n = 1; n <= tilesTotal; n++) {
-  let y = Math.ceil(n / tilesX);
-  let x = n - (y - 1) * 9;
-  tiles.push({ y, x });
+  let y = Math.ceil(n / tilesX); // put tiles on correct row by finding number of tiles in row
+  let x = n - (y - 1) * tilesX;
+  let mine = false;
+  tiles.push({ x, y, mine });
 }
-console.log(tiles);
+
+// create canvas
+let cnv = document.getElementById("gameCnv");
+let ctx = cnv.getContext("2d");
+// set size to number of tiles * size of tiles
+cnv.width = tilesX * tileSize;
+cnv.height = tilesY * tileSize;
 
 drawGame();
 function drawGame() {
@@ -22,14 +29,44 @@ function drawGame() {
   }
 }
 
+// fill full canvas with color
 function fillCanvas(color) {
   ctx.fillStyle = color;
   ctx.fillRect(0, 0, cnv.width, cnv.height);
 }
 
+// draw tile at (x, y) with color
 function drawTile(x, y) {
-  ctx.fillStyle = "white";
-  ctx.strokeStyle = "gray";
-  ctx.fillRect((x - 1) * 50, (y - 1) * 50, 50, 50);
-  ctx.strokeRect((x - 1) * 50, (y - 1) * 50, 50, 50);
+  ctx.fillStyle = "grey";
+  ctx.strokeStyle = "white";
+  ctx.fillRect((x - 1) * tileSize, (y - 1) * tileSize, tileSize, tileSize);
+  ctx.strokeRect((x - 1) * tileSize, (y - 1) * tileSize, tileSize, tileSize);
 }
+
+function initCanvas(firstTile) {
+  for (let n = 1; n <= mineNum; n++) {
+    generateMine(firstTile);
+  }
+}
+
+function generateMine(tileExclusion) {
+  let mineLocation = tiles[randomInt(0, tiles.length - 1)];
+  if (
+    mineLocation.x === tileExclusion.x &&
+    mineLocation.y === tileExclusion.y
+  ) {
+    generateMine(tileExclusion);
+  } else if (mineLocation.mine === true) {
+    generateMine(tileExclusion);
+  } else {
+    mineLocation.mine = true;
+  }
+}
+
+cnv.onmousedown = function (ev) {
+  let tileClicked = {
+    x: Math.ceil(ev.layerX / tileSize),
+    y: Math.ceil(ev.layerY / tileSize),
+  };
+  if (!isGameStarted) initCanvas(tileClicked);
+};
